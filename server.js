2becded5b45cc32491e2f2942758d085ef155c31
nde95 <ndennison268@gmail.com>
2023-04-14 07:24:42 -0400

    adding files

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+
+const app = express();
+app.use(bodyParser.json());
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'Jacob',
+            email: 'Jacob@gmail.com',
+            password: 'johnjacob',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Terry',
+            email: 'Terry@gmail.com',
+            password: 'terryjacob',
+            entries: 0,
+            joined: new Date()
+        },
+    ]
+}
+
+app.get('/', (req, res) => {
+    res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {
+   if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
+    res.json('success');
+   } else {
+    res.status(400).json('error logging in');
+   }
+})
+
+
+app.post('/register', (req,res) => {
+    const { email, name, password } = req.body;
+    database.users.push({
+        id: '125',
+        name: name,
+        email: email,
+        password: password,
+        entries: 0,
+        joined: new Date()
+    })
+    res.json(database.users[database.users.length-1]);
+})
+
+
+app.listen(3000, ()=> {
+    console.log('app is running on 3000')
+})
+
+
+/*
+/signin --> POST
+/register --> POST
+/profile/:userid --> GET = user
+/image --> PUT because user exists--> updated score
+
+
+*/
