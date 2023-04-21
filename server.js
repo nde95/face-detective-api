7e08b854fb12f9fd946d9788abaae4e6bb183fcc
nde95 <ndennison268@gmail.com>
2023-04-21 16:10:17 -0400

    relocating register

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ const cors = require('cors');
 const bcrypt = require('bcrypt-nodejs');
 
 const register = require('./controllers/register');
-const signin = require('./controllers/signin');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image');
 
 const db = require('knex')({
     client: 'pg',
@@ -52,34 +49,7 @@ app.post('/signin', (req, res) => {
 })
 
 
-app.post('/register', (req,res) => {
-    const { email, name, password } = req.body;
-    const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-        trx.insert({
-            hash: hash,
-            email: email
-        })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-            return trx('users')
-                .returning('*')
-                .insert({
-                email: loginEmail[0].email,
-                name: name,
-                joined: new Date()
-    })
-    .then(user => {
-        res.json(user[0]);
-    })
-    })
-     .then(trx.commit)
-     .catch(trx.rollback)
-    })
-   
-    .catch(err => res.status(400).json('Error registering account, please try again!'))
-})
+app.post('/register', (req,res) => { register.handleRegister(req, res, db, bcrypt) } )
 
 
 app.get('/profile/:id', (req, res) => {
