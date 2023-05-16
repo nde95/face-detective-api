41499b417dd8bfd70e4ed830ad201e496d04db1b
nde95 <ndennison268@gmail.com>
2023-05-15 21:59:35 -0400

    listener update

--- server.js
+++ server.js
@@ -41,6 +41,6 @@ app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
   
 
 app.listen(process.env.PORT || 3000, ()=> {
-    console.log('app is running on 3000')
+    console.log(`app is running on ${process.env.PORT}`)
 })
 
